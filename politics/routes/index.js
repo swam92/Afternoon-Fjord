5e1ee200e3f7b77d2465b7c4613ec96a8e22fa26
swam92 <svw223@nyu.edu>
2015-06-13 13:41:34 -0400

    stripped things down for deployement

--- politics/routes/index.js
+++ politics/routes/index.js
@@ -4,25 +4,9 @@ var mongoose = require('mongoose');
 var request = require('request');
 var api = require('sunlight-congress-api')
 
-representativesDatabase = mongoose.model('RepresentativesContact');
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-	var url = "http://openstates.org/api/v1/legislators/?apikey=f8f5270cbe6a457398849e49d1b46070"
-	
-	request({
-    	url: url,
-    	json: true
-	}, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-        res.render('index', { info: body });
-    }
-	
-	})//end request
-
+    res.render('index', { info: "welcome" });
 });
 
 //https://sunlightlabs.github.io/congress/bills.html#votes
