6beac67a78e393a3856b0dd204777b3bd59be996
swam92 <svw223@nyu.edu>
2015-06-13 16:53:01 -0400

    some basic scaffolding

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,18 @@ router.get('/', function(req, res, next) {
     res.render('index', { info: "welcome" });
 });
 
+router.get('/information', function(req,res,next){
+	res.render('information');
+});
+
+router.get('/polls', function(req,res,next){
+	res.render('polls');
+});
+
+router.get('/news', function(req,res,next){
+	res.render('news');
+});
+
 //https://sunlightlabs.github.io/congress/bills.html#votes
 
 module.exports = router;
