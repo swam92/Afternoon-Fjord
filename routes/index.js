d0e1c5b29a50012bed86f26b5b0211c767ad7e0b
swam92 <svw223@nyu.edu>
2015-06-13 16:27:55 -0400

    gonna work

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var request = require('request');
-var api = require('sunlight-congress-api')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
