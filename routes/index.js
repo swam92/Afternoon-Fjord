c4faf6fb774e949c60f99eeb857b1d8e313b6e31
swam92 <svw223@nyu.edu>
2015-06-14 13:06:08 -0400

    added nyt feed

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var request = require('request');
+var request = require('request');
 var router = express.Router();
 
 
@@ -12,19 +12,24 @@ router.get('/information', function(req,res,next){
 	res.render('information');
 });
 
-router.get('/polls', function(req,res,next){
-	/*request('http://elections.huffingtonpost.com/pollster/api/charts/obama-job-approval.json', function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-  		console.log(body);
-    	res.render('polls', {info: body});
-  	}
-})
-	*/
+router.get('/polls', function(req,res,next){	
 	res.render('polls');
 });
 
 router.get('/news', function(req,res,next){
-	res.render('news');
+		request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=jeb+bush+hillary+clinton&sort=newest&api-key=a243d1a3bb2631b740343a2128465629:0:43533076', function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+  		var parsedResponse = JSON.parse(body)
+  		var localResponse = parsedResponse.response
+		var articles = localResponse["docs"] 
+  		articles.forEach(function (article){
+  			console.log(article.headline.main);
+  		})
+
+  		res.render('news', {articles: articles});
+
+  	}
+});
 });
 
 //https://sunlightlabs.github.io/congress/bills.html#votes
