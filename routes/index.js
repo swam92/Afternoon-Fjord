5699201b6aad68f1ec0afefe974ad82a807ff5eb
swam92 <svw223@nyu.edu>
2015-06-14 14:44:14 -0400

    added thing

--- routes/index.js
+++ routes/index.js
@@ -17,19 +17,32 @@ router.get('/polls', function(req,res,next){
 });
 
 router.get('/news', function(req,res,next){
-		request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=jeb+bush+hillary+clinton&sort=newest&api-key=a243d1a3bb2631b740343a2128465629:0:43533076', function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-  		var parsedResponse = JSON.parse(body)
-  		var localResponse = parsedResponse.response
-		var articles = localResponse["docs"] 
-  		articles.forEach(function (article){
-  			console.log(article.headline.main);
-  		})
+	var api_key = 's_557cdf1d6cd93557cdf1d6ce08557cdf1d6ce7b',
+    url = 'cnn.com/politics';
 
-  		res.render('news', {articles: articles});
+    //"https://embedkit.com/api/v1/extract?url=<required>&api_key=<SECRET>")
 
-  	}
-});
+    request('https://embedkit.com/api/v1/extract?url='+url+'&api_key='+api_key, function (error, response, body) {
+  		if (!error && response.statusCode == 200) {
+    		console.log(body);
+  		}
+	});
+
+	request('https://embedkit.com/api/v1/embed?api_key='+api_key+'&url='+url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body);
+	  }
+	});    
+
+	request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=jeb+bush+hillary+clinton&sort=newest&api-key=a243d1a3bb2631b740343a2128465629:0:43533076', function (error, response, body) {
+
+  		if (!error && response.statusCode == 200) {
+  			var parsedResponse = JSON.parse(body)
+  			var localResponse = parsedResponse.response
+			var articles = localResponse["docs"] 
+  			res.render('news', {articles: articles});
+  		}
+	});
 });
 
 //https://sunlightlabs.github.io/congress/bills.html#votes
