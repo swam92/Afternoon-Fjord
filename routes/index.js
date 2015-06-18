a8704a780d8d83c9461ef4400ffc7546b2079b16
swam92 <svw223@nyu.edu>
2015-06-17 23:37:40 -0400

    again

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-var newsQuery = require('newsQuery');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
