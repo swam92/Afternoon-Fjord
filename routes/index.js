09c1d6dfc8fb541ce25735e28195b5f93a1b2f5f
swam92 <svw223@nyu.edu>
2015-06-17 23:36:15 -0400

    changed layout adding laws

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var request = require('request');
 var router = express.Router();
-
+var newsQuery = require('newsQuery');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,33 +19,23 @@ router.get('/polls', function(req,res,next){
 router.get('/news', function(req,res,next){
 	var api_key = 's_557cdf1d6cd93557cdf1d6ce08557cdf1d6ce7b',
     url = 'cnn.com/politics';
-
-    //"https://embedkit.com/api/v1/extract?url=<required>&api_key=<SECRET>")
-
-    request('https://embedkit.com/api/v1/extract?url='+url+'&api_key='+api_key, function (error, response, body) {
-  		if (!error && response.statusCode == 200) {
-    		console.log(body);
-  		}
-	});
-
-	request('https://embedkit.com/api/v1/embed?api_key='+api_key+'&url='+url, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    console.log(body);
-	  }
-	});    
-
+    var articleCall;
+  
 	request('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=jeb+bush+hillary+clinton&sort=newest&api-key=a243d1a3bb2631b740343a2128465629:0:43533076', function (error, response, body) {
 
   		if (!error && response.statusCode == 200) {
   			var parsedResponse = JSON.parse(body)
   			var localResponse = parsedResponse.response
 			var articles = localResponse["docs"]
-			console.log(body); 
-  			res.render('news', {articles: articles});
+			//console.log(body); 
+  			res.render('news', {articles: articles, articlesTwo: articleCall});
   		}
 	});
 });
 
+router.get('/laws', function(req,res,next){
+	res.render('laws');
+})
 //https://sunlightlabs.github.io/congress/bills.html#votes
 
 module.exports = router;
